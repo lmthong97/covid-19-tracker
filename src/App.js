fbc4921ca9e3262c94d0a39a6e62367b14cf348b
lmthong97 <lmthong97@gmail.com>
2021-06-03 17:59:08 +0700

    covid-19-tracking

--- src/App.js
+++ src/App.js
@@ -1,18 +1,86 @@
-import React from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 import CountrySelector from './components/CountrySelector';
 import Highlight from './components/Highlight';
 import Summary from './components/Summary';
+import { getCountries, getReportByCountry } from './apis';
+import sortBy from 'lodash.sortby';
+import '@fontsource/roboto';
+import moment from 'moment';
+import 'moment/locale/vi';
+import { Container, Typography } from '@material-ui/core';
 
-
+moment.locale('vi');
 
 function App() {
-  return (
-    <div className="App">
-      <CountrySelector/>
-      <Highlight/>
-      <Summary/>
+  const [countries, setCountries] = useState([]);
+  const [selectedCountryId, setSelectedCountryId] = useState('');
+  const [report, setReport] = useState([]);
 
-    </div>
+  useEffect(() => {
+    getCountries().then((res) => {
+      const { data } = res;
+      const countries = sortBy(data, 'Country');
+      setCountries(countries);
+      setSelectedCountryId('vn');
+    });
+  }, []);
+
+  const handleOnChange = React.useCallback((e) => {
+    setSelectedCountryId(e.target.value);
+  }, []);
+
+  useEffect(() => {
+    if (selectedCountryId) {
+      const selectedCountry = countries.find(
+        (country) => country.ISO2 === selectedCountryId.toUpperCase()
+      );
+      getReportByCountry(selectedCountry.Slug).then((res) => {
+        // console.log('getReportByCountry', { res });
+        // remove last item = current date
+        res.data.pop();
+        setReport(res.data);
+      });
+    }
+  }, [selectedCountryId, countries]);
+
+  const summary = useMemo(() => {
+    if (report && report.length) {
+      const latestData = report[report.length - 1];
+      return [
+        {
+          title: 'Số ca nhiễm',
+          count: latestData.Confirmed,
+          type: 'confirmed',
+        },
+        {
+          title: 'Khỏi',
+          count: latestData.Recovered,
+          type: 'recovered',
+        },
+        {
+          title: 'Tử vong',
+          count: latestData.Deaths,
+          type: 'death',
+        },
+      ];
+    }
+    return [];
+  }, [report]);
+
+  return (
+    <Container style={{ marginTop: 20 }}>
+      <Typography variant="h2" component="h2">
+        Số liệu COVID-19
+      </Typography>
+      <Typography>{moment().format('LLL')}</Typography>
+      <CountrySelector
+        handleOnChange={handleOnChange}
+        countries={countries}
+        value={selectedCountryId}
+      />
+      <Highlight summary={summary} />
+      <Summary countryId={selectedCountryId} report={report} />
+    </Container>
   );
 }
 
--- src/components/Summary/index.js
+++ src/components/Summary/index.js
@@ -1,9 +1,32 @@
-import React from 'react'
+import { Grid } from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
+import { getMapDataByCountryId } from '../../apis';
+import HighMaps from '../Charts/HighMaps';
+import LineChart from '../Charts/LineChart';
 
-export default function Summary() {
-    return (
-        <div>
-            This is Summary
-        </div>
-    )
+export default function Summary({ report, countryId }) {
+  const [mapData, setMapData] = useState({});
+
+  useEffect(() => {
+    if (countryId) {
+      getMapDataByCountryId(countryId)
+        .then((res) => {
+          setMapData(res);
+        })
+        .catch((err) => console.log({ err }));
+    }
+  }, [countryId]);
+
+  return (
+    <div style={{ height: '500px', marginTop: 10 }}>
+      <Grid container spacing={3}>
+        <Grid item sm={8} xs={12}>
+          <LineChart data={report} />
+        </Grid>
+        <Grid item sm={4} xs={12}>
+          <HighMaps mapData={mapData} />
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
